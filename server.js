ef01872e9a9f2fcb07c4d3defdf3d6c6f7fee577
caesyDev <kenechukwuokafor7@gmail.com>
2026-04-18 10:41:15 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+//build a simple express server
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/api/patient', (req, res) => {
+  console.log(req.body);
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
